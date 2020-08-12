2b60b6bd3d7275042d470a5fb48bcd9e9e2fea97
Gremis <18993990tovargremis@gmail.com>
2020-08-12 19:46:20 -0300

    create all

---
+++ app/database/conect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = 'test';
+$db_name = 'blog';
+
+
+$conexion = new mysqli($host, $user, $pass, $db_name);
+
+if ($conexion->connect_error){
+    die('Database connection error: '. $conexion->connect_error);
+}
